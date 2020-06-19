c5a7ec346204611adb21aa7a7a95152058d616bd
ap <aryan.pandhi@gmail.com>
2020-06-19 08:41:42 +0530

    coded oneEditAway()

--- basic/Arrays.java
+++ basic/Arrays.java
@@ -11,15 +11,20 @@ class Arrays{
         assert isPermutation("happy", "happo");
         assert isPermutation("", "");
         assert isPermutation("lol", "lol");
-        /* urlify Tests */
-        assert urlify("ok hi hello").equals("ok%20hi%20hello");
-        assert urlify("").equals("");
-        assert urlify("apple").equals("apple");
-        /* isPalPer Tests */
-        assert !isPalPer("hello world");
-        assert isPalPer("Tact Coa");
-        assert isPalPer("");
-
+        /* replaceSpaces Tests */
+        assert replaceSpaces("ok hi hello").equals("ok%20hi%20hello");
+        assert replaceSpaces("").equals("");
+        assert replaceSpaces("apple").equals("apple");
+        /* isPermutationOfPalindrome Tests */
+        assert !isPermutationOfPalindrome("hello world");
+        assert isPermutationOfPalindrome("Tact Coa");
+        assert isPermutationOfPalindrome("");
+        /* oneEditAway Tests */
+        assert oneEditAway("coke", "come");
+        assert !oneEditAway("b", "bee");
+        assert oneEditAway("", "");
+        assert oneEditAway("to","toe");
+        assert oneEditAway("bee", "be");
         System.out.println("All Tests Passed!");
     }
 
@@ -53,7 +58,7 @@ class Arrays{
         return true;
     }
 
-    static String urlify(String str){
+    static String replaceSpaces(String str){
         StringBuilder output = new StringBuilder();
 
         for(int i=0; i<str.length();i++){
@@ -69,7 +74,7 @@ class Arrays{
 
     }
 
-    static boolean isPalPer(String str){
+    static boolean isPermutationOfPalindrome(String str){
         str.toLowerCase();
 
         int[] count = new int[128];
@@ -87,7 +92,32 @@ class Arrays{
         }
 
         return true;
+    }
 
+    static boolean oneEditAway(String s, String t){
+        if (java.lang.Math.abs(s.length()-t.length())>1) return false;
+
+        String s1 = s.length() < t.length() ? s : t;
+        String s2 = s.length() < t.length() ? t : s;
+        boolean diffChar = false;
+        int index1 = 0;
+        int index2 = 0;
+
+        while(index1 < s1.length()){
+            if (s1.charAt(index1) != s2.charAt(index2)){
+
+                if (diffChar) return false;
+                diffChar = true;
+
+                if (s1.length() == s2.length()) index1++;
+
+            } else {
+                index1++;
+            }
+            index2++;
+        }
+
+        return true;
     }
 
 }
