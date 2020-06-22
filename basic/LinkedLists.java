b14b19748ab79eafdd626092f01db8f4bffe4bc4
ap <aryan.pandhi@gmail.com>
2020-06-22 10:51:07 +0530

    added a function

--- basic/LinkedLists.java
+++ basic/LinkedLists.java
@@ -12,6 +12,11 @@ class LinkedLists{
         }
         Collections.shuffle(li);
         deleteDups(li);
+        LinkedList<Integer> lu = new LinkedList<>();
+        for(int i = 0; i<5;i++){
+            lu.add(i);
+        }
+        assert klast(lu,2) == 3;
     }
 
     static void deleteDups(LinkedList<Integer> n){
@@ -25,6 +30,15 @@ class LinkedLists{
                 set.add(i);
             }
         }
+    }
+
+    static Integer klast(LinkedList<Integer> n, int k){
+        Iterator<Integer> iterator = n.iterator();
+        int index = n.size() - k + 1;
+        for (int i = 0; i < index - 1; i++){
+            iterator.next();
+        }
+        return iterator.next();
     }
 
 }
